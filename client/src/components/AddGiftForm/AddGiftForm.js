3a15951262c06ce661bf6a35d4076015edd2752d
Frances Hitchcock <fran.h.hitchcock@gmail.com>
2023-08-07 10:03:28 +0100

    add gift form component added

---
+++ client/src/components/AddGiftForm/AddGiftForm.js
@@ -0,0 +1,9 @@
+import "./AddGiftForm.css";
+
+export default function AddGiftForm() {
+  return (
+    <div className="add-gift-form-container">
+      <h3>I am an add gift form</h3>
+    </div>
+  );
+}
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import "./Home.css";
+import AddGiftForm from "../../components/AddGiftForm/AddGiftForm";
 
 export default function Home() {
   return (
     <main>
       <div className="container home-container">
         <h2>I am the home page</h2>
+        <AddGiftForm />
       </div>
     </main>
   );
