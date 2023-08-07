176951af5381390eb2f3796fbf6e2be6f3665af6
Frances Hitchcock <fran.h.hitchcock@gmail.com>
2023-08-07 10:22:17 +0100

    browser router imported and route paths added in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import "./reset.css";
 import "./App.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import About from "./pages/About/About";
@@ -11,15 +13,19 @@ import Admin from "./pages/Admin/Admin";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <About />
-      <Gifts />
-      <GiftById />
-      <Admin />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/gifts" element={<Gifts />} />
+          <Route path="/gift/:id" element={<GiftById />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
