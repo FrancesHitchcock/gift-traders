374158378c31f49fcbc97a408b3bc4165ec64552
Frances Hitchcock <fran.h.hitchcock@gmail.com>
2023-08-07 15:11:00 +0100

    Gift page updated, LocationIQ updated

--- client/src/pages/GiftById/GiftById.js
+++ client/src/pages/GiftById/GiftById.js
@@ -7,10 +7,18 @@ export default function GiftById({ gifts }) {
 
   const targetGift = gifts.find((gift) => gift._id === paramsId);
 
+  // const location = targetGift.location
+  const location = "Halesworth";
+
   return (
     <main>
       <div className="container gift-by-id-container">
-        <h2>{targetGift.giftName}</h2>
+        <h2 className="selected-gift-h2">Gift: {targetGift.giftName}</h2>
+        <h3>Department: {targetGift.type}</h3>
+        <img src={targetGift.img_url} alt={targetGift.giftName} />
+        <p>{targetGift.description}</p>
+        <h3>Suggested donation: £{targetGift.donation.toFixed(2)}</h3>
+        <h3>Pick-up location: {targetGift.address}</h3>
       </div>
     </main>
   );
