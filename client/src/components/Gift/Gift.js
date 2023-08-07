3a2490e8f8b65cf70d01773e712aa55f74008896
Frances Hitchcock <fran.h.hitchcock@gmail.com>
2023-08-07 13:15:51 +0100

    resolved display issue

--- client/src/components/Gift/Gift.js
+++ client/src/components/Gift/Gift.js
@@ -3,12 +3,15 @@ import "./Gift.css";
 export default function Gift({ gift }) {
   return (
     <div className="gift-container">
-      <h3>Gift: {gift.giftName[0].toUpperCase() + gift.giftName.slice(1)}</h3>
+      <h3 className="gift-name">
+        Gift: {gift.giftName[0].toUpperCase() + gift.giftName.slice(1)}
+      </h3>
       <p className="gift-type">
         Department: {gift.type[0].toUpperCase() + gift.type.slice(1)}
       </p>
       <div className="gift-image-container">
         <img
+          className="gift-image"
           src={gift.img_url}
           alt={gift.giftName[0].toUpperCase() + gift.giftName.slice(1)}
         />
