96b252123d99e7b21edbc5fd4ec33d0b19896be6
Frances Hitchcock <fran.h.hitchcock@gmail.com>
2023-08-07 11:32:49 +0100

    updated gift with dummy data

--- client/src/components/Gift/Gift.js
+++ client/src/components/Gift/Gift.js
@@ -1,9 +1,13 @@
 import "./Gift.css";
 
-export default function Gift() {
+export default function Gift({ gift }) {
   return (
     <div className="gift-container">
-      <h3>I am a gift</h3>
+      <h3>{gift.giftName}</h3>
+      <p className="gift-type">{gift.type}</p>
+      <div className="gift-image-container">
+        <img src={gift.img_url} alt={gift.giftName} />
+      </div>
     </div>
   );
 }
