5597a506533977ce70e44ab79079da2b1eaadfd8
Katy <katy.ledgard@gmail.com>
2023-08-10 13:55:53 +0100

    update login buttons

--- client/src/components/LogoutButton/LogoutButton.js
+++ client/src/components/LogoutButton/LogoutButton.js
@@ -1,11 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import "./LogoutButton.css";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
     <button
+      className="login-btn"
       onClick={() =>
         logout({ logoutParams: { returnTo: window.location.origin } })
       }
